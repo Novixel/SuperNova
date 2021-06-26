830ff353d451ab027953ef52ebb5e3a66f61c3fd
Novixel <novixel@hotmail.com>
2021-06-26 03:02:27 -0700

    0.05
    added Some Trading Functions

--- data/manager.py
+++ data/manager.py
@@ -1,3 +1,4 @@
+from cbpro.authenticated_client import AuthenticatedClient
 from data.items import AccountItem , ProductItem
 import time
 from datetime import datetime
@@ -8,18 +9,31 @@ class Manager():
     Provide Me With An Authenticated User!
     
     '''
+    # Constants
+    BUY = 'buy'
+    SELL = 'sell'
+    ATH = 64899.00
+    
+    # Lists
     Accounts : list = []
     Products : list = []
     BTCPairs : list = []
     NONPairs : list = []
 
+    # Active List
     Available : list = []
+
+    # Trade Variables
+    last_trade_id : str = 'trade_id'
+    last_trade_price : float = 0
+    last_trade_side : BUY or SELL
+    last_trade_size : float = 0
 
     def __init__(self, connect):
         '''Connection Object, opt: True  = auto getTotals'''
         self.Connection = connect
         self.User = self.Connection.name
-        self.Client = self.Connection.client()
+        self.Client : AuthenticatedClient = self.Connection.client()
         self.__getAccounts()
         self.__getProducts()
         self.__getTradable()
@@ -49,7 +63,77 @@ class Manager():
                 print("Tradable product error :",self.Products[i])
                 raise ValueError
 
+    def ShowAccounts(self):
+        '''Show All Accounts!'''
+        for i in range(len(self.Accounts)):
+           print(i, self.Accounts[i].currency)
+
+    def ShowProducts(self):
+        '''Show All Products!'''
+        for i in range(len(self.Products)):
+           print(i, self.Products[i].id)
+    
+    def ShowBTCPairs(self):
+        '''Show Sorted BTC Product Pairs!'''
+        for i in range(len(self.BTCPairs)):
+           print(i, self.BTCPairs[i].id)
+
+    def ShowNONPairs(self):
+        '''Show Sorted NON BTC Product Pairs!'''
+        for i in range(len(self.NONPairs)):
+           print(i, self.NONPairs[i].id)
+
+    def ShowAvailable(self):
+        '''Show All Available Products!'''
+        for i in range(len(self.Available)):
+           print(i,"%.8f"%self.Available[i].available, self.Available[i].currency)
+
+    def Trade(self, product_id, side, price, size):
+        '''Send Limit Trade Request'''
+        # Place The Trade
+        trade = self.Client.place_limit_order(product_id, side, price, size)
+        # Set Recent Trade Variables
+        self.last_trade_id = str(trade["id"])
+        self.last_trade_side = str(trade["side"])
+        self.last_trade_price = float(trade["price"])
+        self.last_trade_size = float(trade["size"])
+        return trade
+
+    def Quick_Trade(self, product_id:str, side, size):
+        # Get Current Market Price
+        price = float(self.Client.get_product_ticker(product_id)['price'])
+        # Place The Trade
+        if side == self.BUY:
+            trade = self.Trade(product_id, self.BUY, price, size)
+            order = self.Client.get_order(trade["id"])
+            return order
+        elif side == self.SELL:
+            trade = self.Trade(product_id, self.SELL, price, size)
+            order = self.Client.get_order(trade["id"])
+            return order
+
+    def Safe_Trade(self, product_id, side, price, size):
+        # Check The Order
+        if self.last_trade_id != 'trade_id':
+            last_order = self.Client.get_order(self.last_trade_id)
+        # and Status
+        if last_order['status'] == 'open':
+            print("last order is open!")
+            return last_order
+        elif last_order['status'] == 'done':
+            print("last order is done")
+        else:
+            trade = self.Trade(product_id,side,price,size)
+            # Grab That Trade From The Order Book
+            return self.Client.get_order(trade["id"])
+
     def check_Pair(self, product_id):
+        '''Check Product Pair Before You Trade! 
+        -> BaseItem, QuoteItem, ProductItem'''
+        for i in range(len(self.Products)):
+           if product_id == self.Products[i].id:
+               product : ProductItem = self.Products[i]
+
         if any(p.id == product_id for p in self.BTCPairs):
             BTCPair = True
         else:
@@ -67,15 +151,27 @@ class Manager():
             b,q =  product_id.split("-")
             for i in self.Available:
                 if i.currency == b:
-                    base = i
+                    base : AccountItem = i
                 if i.currency == q:
-                    quote = i
+                    quote : AccountItem = i
             print("\tAvailable For Trade!")
             print("\t%.8f"%base.available, base.currency)
             print("\t%.8f"%quote.available, quote.currency)
             print(base.currency,"/",quote.currency,"pair : BTC | NON\n\t        ",BTCPair,"|",NONPair)
+            print("Minumum Trade Size:",product.base_min_size,base.currency)
+        else:
+            raise ValueError("Product Error!",product_id + " is not a valid product!")
 
-        return base , quote , [BTCPair, NONPair]
+        return base, quote, product
+
+    def getBalance(self, base:AccountItem, quote:AccountItem, product_id:str):
+        '''Get balances for product'''
+        base_bal, quote_bal = base.available, quote.available
+        price = self.Client.get_product_ticker(product_id)
+        start_bal = base_bal + (quote_bal / float(price["price"]))
+        print("(BASE)  Total Balance:",start_bal, base.currency)
+        print("(QUOTE) Total Balance:",start_bal * float(price["price"]), quote.currency)
+        return base_bal, quote_bal , start_bal
 
 
     def make_trade(self, product_pair, side, size, price):
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # we import this to set enviroment variables (provides for easy switching of portfolios)
 
 # ### Debug #########
+from data.items import AccountItem, ProductItem
 from dataclasses import asdict
 import envVars.tenv #
 #####################
@@ -10,30 +11,43 @@ from data.connect import Connect
 from data.manager import Manager
 from data.user import User
 
+
 def main():
     '''User() + Connect(User.name, User.api) = Authenticated_Client -> returns Manager(Authenticated_Client)'''
     user = User() # Collect User Data From Enviroment
     con = Connect(user.name, user.api ) # Connect User To Coinbase API
-    man = Manager(con) # Manage Users Coinbase Portfolio
     # Return The Connected Manager
-    return man
+    return Manager(con)
+
+def fills(x:Manager,base:AccountItem, quote:AccountItem, product:ProductItem):
+    fills = x.Client.get_fills(product.id)
+    for i in fills:
+        last_filled = i
+        break
+    print(last_filled["price"])
+
 
 if __name__ == "__main__":
-    # Create The Manager
+    # Call The Manager To Work
     x = main()
 
-    # Set Product For Trade!
+    # Set Product For Trade
     PRODUCT_ID = "BTC-USDC"
 
-    # Grab The Accounts We Are Gonna Use!
-    base, quote, pairs,  = x.check_Pair(PRODUCT_ID)
-                    
+    # Grab The Accounts From Our Product Pair
+    base, quote, product  = x.check_Pair(PRODUCT_ID)
 
-    # print(x.NONPairs)
-    # print(x.BTCPairs)
+    # Get Mininum Trade Size
+    BASEMIN = product.base_min_size
+    QUOTEMIN = product.min_market_funds
 
-    # for i in range(len(x.Accounts)):
-    #    print(i, x.Accounts[i].currency)
+    # Make Test Trade
+    for i in range(3):
+        trade_order = x.Attempt_Trade(product_id=PRODUCT_ID, side=x.SELL, price=x.ATH ,size=BASEMIN)
+        print(trade_order)
+    # We Can Grab Balances Seperatly If We Need to Do Other Math
+    # base_bal, quote_bal , start_bal = x.getBalance(base, quote, PRODUCT_ID)
 
-    # for i in range(len(x.Products)):
-    #    print(i, x.Products[i].id)
+
+
+    
